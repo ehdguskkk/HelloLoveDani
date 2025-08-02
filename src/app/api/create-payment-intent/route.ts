50cc0bbd7118ab8180920bae3cdf78cb6b1fb5c7
ehdguskkk <sae4762@gmail.com>
2025-08-02 15:36:33 +1000

    fix: stripe payment intent api

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -1,24 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import Stripe from 'stripe';
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
+export async function POST(req: NextRequest) {
+  const { amount } = await req.json();
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log("STRIPE_SECRET_KEY ENV:", process.env.STRIPE_SECRET_KEY);
-  if (req.method === 'POST') {
-    const { amount } = req.body;
-    try {
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount,
-        currency: 'aud',
-        payment_method_types: ['card'],
-      });
-      res.status(200).json({ clientSecret: paymentIntent.client_secret });
-    } catch (err: any) {
-  res.status(500).json({ error: err.message });
-  }
-  } else {
-    res.status(405).end();
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'aud',
+      payment_method_types: ['card'],
+    });
+
+    return NextResponse.json({ clientSecret: paymentIntent.client_secret });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
   }
 }
